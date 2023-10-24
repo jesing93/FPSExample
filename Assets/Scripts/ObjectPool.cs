cabf30e7ffac6a42cad6020e14a36de2cf060907
Jesing <jesing93@gmail.com>
2023-10-24 11:22:19 +0200

    Initial Commit

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabObject;
+    [SerializeField] private int objectNumberOnStart;
+
+    private List<GameObject> poolObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        //Instantiate the objects
+        for (int i = 0; i < objectNumberOnStart; i++)
+        {
+            CreateNewObject();
+        }
+    }
+
+    /// <summary>
+    /// Instantiate object and add it to the list
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    private GameObject CreateNewObject()
+    {
+        GameObject gameObject = Instantiate(prefabObject);
+        gameObject.SetActive(false); //Deactivate
+        poolObjects.Add(gameObject); //Add to the list
+
+        return gameObject;
+    }
+
+    /// <summary>
+    /// Take from the list an available object or create one there is none available
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetGameObject()
+    {
+        //Find in the poolObject an object that is Inactive in the game
+        GameObject gameObject = poolObjects.Find(x => x.activeInHierarchy == false);
+        //If not found, create object
+        if (gameObject == null)
+        {
+            gameObject = CreateNewObject();
+        }
+        //Activate the selected object
+        gameObject.SetActive(true);
+
+        return gameObject;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Camera")]
+    [SerializeField] private float mouseSensibility;
+    [SerializeField] private float maxViewX;
+    [SerializeField] private float minViewX;
+    private float rotationX;
+
+    //Components
+    private Camera pCamera;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        pCamera = Camera.main;
+
+        //Hide the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraView();
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    /// <summary>
+    /// Movement action
+    /// </summary>
+    private void MovePlayer()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = (transform.right * x + transform.forward * z).normalized * speed;
+        direction.y = rb.velocity.y;
+
+        rb.velocity = direction;
+    }
+
+    /// <summary>
+    /// Jump action
+    /// </summary>
+    private void Jump()
+    {
+        //Cast a ray down from player position
+        Ray ray = new Ray(transform.position, Vector3.down);
+        
+        //If collides, can jump
+        if (Physics.Raycast(ray, 1.1f))
+        {
+            rb.velocity = new Vector3 (rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+
+    /// <summary>
+    /// The controller of the camera movement (Uses mouse sensibility)
+    /// </summary>
+    private void CameraView()
+    {
+        //Get from Mouse Input X and Y axis
+        float y = Input.GetAxis("Mouse X") * mouseSensibility;
+        rotationX += Input.GetAxis("Mouse Y") * mouseSensibility;
+
+        //Cut x rotation with min and max limits
+        rotationX = Mathf.Clamp(rotationX, minViewX, maxViewX);
+
+        //Camera rotation
+        pCamera.transform.localRotation = Quaternion.Euler(-rotationX, 0, 0);
+
+        //Rotate the player
+        transform.eulerAngles += Vector3.up * y;
+    }
+}
